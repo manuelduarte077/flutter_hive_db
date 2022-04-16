6ac2f94937914330bfba8e6c7499098ce9839941
@manuelduarte077 <carlosd99m@gmail.com>
2022-04-16 16:51:03 -0600

    :package: agregando los paquetes a usar

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
